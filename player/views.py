480198e28a74f6ec6d5c77fe036ac5f2f804d41c
Stephen Emslie <stephenemslie@gmail.com>
2020-05-26 15:59:22 +0200

    Added RoomCreateView

--- player/views.py
+++ player/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+
+from .models import Room
+
 
 def index(request):
     if request.user.is_authenticated:
@@ -8,6 +14,14 @@ def index(request):
     else:
         return render(request, 'player/login.html')
 
+
 def logout_view(request):
     logout(request)
     return redirect(index)
+
+
+@method_decorator(login_required, name='dispatch')
+class RoomCreateView(CreateView):
+
+    model = Room
+
