6e03ad476759a010dff953f352bc1bd915e2b31f
Stephen Emslie <stephenemslie@gmail.com>
2020-05-26 12:50:46 +0200

    Added player app

--- player/views.py
+++ player/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'player/index.html')
+    else:
+        return render(request, 'player/login.html')
