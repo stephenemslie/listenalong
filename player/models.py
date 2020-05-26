70bdde8b931f4aad0f57cc7f1b06f98e2ee1357c
Stephen Emslie <stephenemslie@gmail.com>
2020-05-26 15:48:42 +0200

    Added the Room model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import string
+from random import SystemRandom
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+def generate_room_slug():
+    choice = SystemRandom().choice
+    chars = string.ascii_uppercase + string.digits
+    return "".join(choice(chars) for i in range(6))
+
+
+class Room(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(default=generate_room_slug)
+
+    def __str__(self):
+        return f'{self.slug} ({self.user.username})'
+
