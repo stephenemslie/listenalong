ef1c27d5e72ab7d93f2fd78ff1fbc2c9efed72e1
Stephen Emslie <stephenemslie@gmail.com>
2020-05-26 12:51:37 +0200

    Added social auth and player urls

--- listenalong/urls.py
+++ listenalong/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
+    path('', include('player.urls'))
 ]
