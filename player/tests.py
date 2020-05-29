9708599a9bba132c07f61dd8e698d414560a96a5
Stephen Emslie <stephenemslie@gmail.com>
2020-05-29 13:38:56 +0200

    Added tekore type factories

--- player/tests.py
+++ player/tests.py
@@ -37,6 +37,52 @@ class RoomFactory(factory.django.DjangoModelFactory):
     progress_ms = 5000
     timestamp = factory.LazyAttribute(lambda a: timezone.now())
 
+
+class SpotifyContextTypeFactory(factory.Factory):
+
+    class Meta:
+        model = MagicMock
+
+    value = "playlist"
+
+
+class SpotifyContextFactory(factory.Factory):
+
+    class Meta:
+        model = MagicMock
+
+    uri = factory.Faker('uuid4')
+    type = factory.SubFactory(SpotifyContextTypeFactory)
+
+
+class SpotifyItemFactory(factory.Factory):
+
+    class Meta:
+        model = MagicMock
+
+    id = factory.Faker('uuid4')
+    uri = factory.Faker('uuid4')
+    _name = factory.Faker('pystr')
+
+    # MagicMock reserves `name` for the constructor, so we need to set it post
+    # generation
+    # https://docs.python.org/3/library/unittest.mock.html#mock-names-and-the-name-attribute
+    @factory.post_generation
+    def set_name(obj, create, extracted, **kwargs):
+        obj.name = obj._name
+
+
+class SpotifyPlayingFactory(factory.Factory):
+
+    class Meta:
+        model = MagicMock
+
+    is_playing = False
+    progress_ms = 1000
+    item = factory.SubFactory(SpotifyItemFactory)
+    context = factory.SubFactory(SpotifyContextFactory)
+
+
 class UserTestCase(TestCase):
 
     @patch.object(tekore.Spotify, 'playback_shuffle')
