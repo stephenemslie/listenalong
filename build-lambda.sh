3cd9ef90b9046177242b62c64dc6ad04f6475728
Stephen Emslie <stephenemslie@gmail.com>
2020-05-30 13:47:43 +0200

    Exit immediately on error

--- build-lambda.sh
+++ build-lambda.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -eux
 zip -r9 package.zip .  -x ".git/*" -x "terraform/*" -x "package/*"
 poetry export -f requirements.txt -o requirements.txt
 pip install --target=./package -r requirements.txt
