71ccf1cf99663ecceb2a4af229e80c366f03622b
Stephen Emslie <stephenemslie@gmail.com>
2020-05-30 13:43:57 +0200

    Execute build in bash script

---
+++ build-lambda.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+zip -r9 package.zip .  -x ".git/*" -x "terraform/*" -x "package/*"
+poetry export -f requirements.txt -o requirements.txt
+pip install --target=./package -r requirements.txt
+cd package
+zip -r9 -g ../package.zip *
+cd ..
