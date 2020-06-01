b49c84710d28f79bcbfeb834945a97cede447c40
Stephen Emslie <stephenemslie@gmail.com>
2020-06-01 13:40:30 +0200

    Added lambda iam policies and role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -40,3 +40,72 @@ resource "aws_iam_role_policy" "cloudwatch" {
   role   = aws_iam_role.apigateway.id
   policy = data.aws_iam_policy_document.cloudwatch.json
 }
+
+
+# Lambda role
+
+resource "aws_iam_role" "django_lambda" {
+  name = "listenalong-django-lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "django_lambda_cloudwatch" {
+  role       = aws_iam_role.django_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "django_lambda_basic" {
+  role       = aws_iam_role.django_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "django_s3" {
+  name = "django-s3"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.listenalong_db.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.listenalong_db.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "django-lambda-s3" {
+  role       = "${aws_iam_role.django_lambda.name}"
+  policy_arn = "${aws_iam_policy.django_s3.arn}"
+}
