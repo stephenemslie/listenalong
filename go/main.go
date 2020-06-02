224cd5e27deba172ec880909d0cf3d8ad719bbe1
Stephen Emslie <stephenemslie@gmail.com>
2020-06-02 18:16:45 +0200

    Added stubbed http listener

--- go/main.go
+++ go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
 
 func main() {
-	fmt.Println("Hello")
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	http.Handle("/", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
