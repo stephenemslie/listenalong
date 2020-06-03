ec009c2cf1b3e6be1bc27892b9a4db5366d91a81
Stephen Emslie <stephenemslie@gmail.com>
2020-06-03 16:16:50 +0200

    Moved auth handlers and init into auth.go

---
+++ go/auth.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/spotify"
+)
+
+var (
+	oauthConfig *oauth2.Config
+)
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := baseTemplate.Clone()
+	t.ParseFiles("templates/login.html")
+	err := t.Execute(w, struct{}{})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func loginInitHandler(w http.ResponseWriter, r *http.Request) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	code := base64.StdEncoding.EncodeToString(b)
+	session := r.Context().Value(sessionKey).(*sessions.Session)
+	session.Values["oauth_state"] = code
+	session.Save(r, w)
+	url := oauthConfig.AuthCodeURL(code)
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func loginCompleteHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	session := ctx.Value(sessionKey).(*sessions.Session)
+	if session.Values["oauth_state"] != r.FormValue("state") {
+		fmt.Println("error: State doesn't match")
+		return
+	}
+	code := r.FormValue("code")
+	tok, err := oauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	session.Values["spotify_token"] = tok
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func requiresAuth(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := r.Context().Value(sessionKey).(*sessions.Session)
+		if session == nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+		fn(w, r)
+	}
+}
+
+func init() {
+	host := os.Getenv("HOST")
+	oauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("SPOTIFY_KEY"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		RedirectURL:  fmt.Sprintf("%s/complete/spotify/", host),
+		Scopes: []string{
+			"user-read-playback-state",
+			"user-modify-playback-state",
+			"user-read-currently-playing",
+			"playlist-read-collaborative",
+		},
+		Endpoint: spotify.Endpoint,
+	}
+	gob.Register(oauth2.Token{})
+}
--- go/main.go
+++ go/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/gob"
 	"fmt"
 	"html/template"
 	"log"
@@ -15,8 +12,6 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/spotify"
 )
 
 type key int
@@ -25,7 +20,6 @@ var (
 	baseTemplate *template.Template
 	sessionStore *sessions.CookieStore
 	sessionKey   key
-	oauthConfig  *oauth2.Config
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,48 +29,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := baseTemplate.Clone()
-	t.ParseFiles("templates/login.html")
-	err := t.Execute(w, struct{}{})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func loginInitHandler(w http.ResponseWriter, r *http.Request) {
-	b := make([]byte, 32)
-	_, err := rand.Read(b)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	code := base64.StdEncoding.EncodeToString(b)
-	session := r.Context().Value(sessionKey).(*sessions.Session)
-	session.Values["oauth_state"] = code
-	session.Save(r, w)
-	url := oauthConfig.AuthCodeURL(code)
-	http.Redirect(w, r, url, http.StatusFound)
-}
-
-func loginCompleteHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	session := ctx.Value(sessionKey).(*sessions.Session)
-	if session.Values["oauth_state"] != r.FormValue("state") {
-		fmt.Println("error: State doesn't match")
-		return
-	}
-	code := r.FormValue("code")
-	tok, err := oauthConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	session.Values["spotify_token"] = tok
-	session.Values["authenticated"] = true
-	session.Save(r, w)
-	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func sessionMiddleware(next http.Handler) http.Handler {
@@ -90,19 +42,6 @@ func sessionMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func requiresAuth(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		session := r.Context().Value(sessionKey).(*sessions.Session)
-		if session == nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
-		}
-		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-			http.Redirect(w, r, "/login", http.StatusFound)
-		}
-		fn(w, r)
-	}
-}
-
 func init() {
 	secret := os.Getenv("SECRET_KEY")
 	sessionStore = sessions.NewCookieStore([]byte(secret))
@@ -112,20 +51,6 @@ func init() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	host := os.Getenv("HOST")
-	oauthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("SPOTIFY_KEY"),
-		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-		RedirectURL:  fmt.Sprintf("%s/complete/spotify/", host),
-		Scopes: []string{
-			"user-read-playback-state",
-			"user-modify-playback-state",
-			"user-read-currently-playing",
-			"playlist-read-collaborative",
-		},
-		Endpoint: spotify.Endpoint,
-	}
-	gob.Register(oauth2.Token{})
 }
 
 func main() {
