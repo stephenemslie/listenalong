296fdee0f069806f2998510f242c5dcd3c91cbad
Stephen Emslie <stephenemslie@gmail.com>
2020-06-04 01:42:50 +0200

    Added a dynamodb User model

---
+++ go/user.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+type User struct {
+	UserID string `dynamo:"user_id,hash"`
+	Name   string `dynamo:"name"`
+}
+
+type UserService struct {
+	db        *dynamo.DB
+	userTable dynamo.Table
+}
+
+func (u *UserService) CreateTable() {
+	err := u.db.CreateTable("users", User{}).Run()
+	if err != nil {
+		fmt.Println("error", err)
+	}
+}
+
+func NewUserService(endpoint string) (*dynamo.DB, *UserService, error) {
+	db, dynamoTable, err := newDynamoTable("users", endpoint)
+	if err != nil {
+		return nil, nil, err
+	}
+	userService := &UserService{
+		db:        db,
+		userTable: dynamoTable,
+	}
+	return db, userService, err
+}
+
+func newDynamoTable(tableName, endpoint string) (*dynamo.DB, dynamo.Table, error) {
+	cfg := aws.Config{
+		Region: aws.String("eu-west-1"),
+	}
+	if endpoint != "" {
+		cfg.Endpoint = aws.String(endpoint)
+	}
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &cfg)
+	table := db.Table(tableName)
+	return db, table, nil
+}
