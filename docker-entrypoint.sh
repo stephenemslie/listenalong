8eeeded0e78094af910abe37723477c708fc6cc6
Stephen Emslie <stephenemslie@gmail.com>
2020-06-10 15:05:47 +0200

    Updated entrypoint to compile cmd/serve/main.go

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = 'gowatch' ]; then
-    exec CompileDaemon --build="go build -o /usr/src/app/bin/listenalong" --include="*.html" --command=./bin/listenalong
+    exec CompileDaemon --build="go build -o /usr/src/app/bin/listenalong ./cmd/serve/main.go" --include="*.html" --command=./bin/listenalong
 fi
 
 exec "$@"
